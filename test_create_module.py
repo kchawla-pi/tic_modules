341e5ba7db7a79f460a29f1ac5f092340744162e
kchawla-pi <kc.insight.pi@gmail.com>
2017-04-07 19:24:18 -0400

    Created working test for create_module.path() in test_create_module.py
      - Transferred create_test_data.py code to test_create_module.py
      - method CreateModuleTest.create_test_data()
      - method CreateModuleTest.test_path()

--- test_create_module.py
+++ test_create_module.py
@@ -4,23 +4,37 @@ import os
 from create_module import path
 # from create_module import if __name__ == '__main__':
 
+
 class CreateModuleTest(unittest.TestCase):
+    def __init__(self):
+
+        def create_test_data(self):
+
+            create_module_path = os.getcwd()  # "C:/Users/kshit/Dropbox/libraries/kc/workspace/kc/tic_modules"
+            tests_path_dirs = create_module_path.split(os.sep)
+            tests_path_dirs.append('tests')
+            tests_path_dirs.append('data')
+            tests_data_path = os.sep.join(tests_path_dirs)
+            tests_subtree_dirs = list()
+
+            for ls_entry in os.scandir(tests_data_path):
+                if ls_entry.name[:4] == 'tic_' and ls_entry.is_dir():
+                    temp = tests_path_dirs + [ls_entry.name, ls_entry.name[4:]]
+                    tests_subtree_dirs.append(temp)
+
+            test_dir_start_idx = len(create_module_path.split(os.sep))
+
+            tests_subtrees = [os.sep.join(dirs[test_dir_start_idx:]) for dirs in tests_subtree_dirs]
+
+        create_test_data(self)
+
+
 
     def test_path(self):
-        create_module_path = os.getcwd()
-            # "C:/Users/kshit/Dropbox/libraries/kc/workspace/kc/tic_modules"
-        path_dirs = create_module_path.split(os.sep)
-        for dir in os.scandir(os.pardir):
-            print(dir)
-        default_path_parts = ['D:\\', 'libraries', 'kc', 'workspace', 'tic', 'kc', 'tic_modules']  # dir names of path in windows
-        default_path = ''
-        test_dirs = ['label', 'tools']
-        for path_dir_ in default_path_parts:
-            default_path = os.path.join(default_path, path_dir_)
-        module_dir_paths = [os.path.join(default_path, test_dir_) for test_dir_ in test_dirs]
-        print(module_dir_paths)
-        cwd = os.getcwd()
-        print('cwd:', cwd)
-        print('def:', default_path)
-        for idx, test_dir_ in enumerate(test_dirs):
-            print(module_dir_paths, path(test_dir_))
+        for case in range(0, len(self.tests_subtrees)):
+            path_fn_returns = path(self.tests_subtrees[case])
+            expected_value = os.sep.join(self.tests_subtree_dirs[case])
+            self.assertEqual(path_fn_returns, expected_value)
+
+
+unittest.main()
