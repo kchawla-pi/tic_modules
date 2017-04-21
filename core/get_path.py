a1b1dfd4dcf136f3393a5d76a8e5ddbc2bc1bf17
Kshitij Chawla <kc.insight.pi@gmail.com>
2017-04-21 12:37:56 -0400

    Moved path() aka get_path() to its own .py for better focus

---
+++ core/get_path.py
@@ -0,0 +1,49 @@
+import os
+import platform
+import time
+import sys
+from pprint import pprint
+
+
+def dp(*arg):
+    print('\n','-'*50, 'Start', '-'*50, '\n', sep='\n')
+    for i in arg:
+        print(i)
+    print('\n', '-' * 50, 'END', '-' * 50, '\n', sep='\n')
+
+
+file_arg = sys.argv
+# if file_arg:
+#     file_arg = os.getcwd()
+dp(file_arg)
+
+
+for i in range(0,len(file_arg)):
+    if file_arg[i]:
+        path_is_abs = file_arg[i] == os.sep or \
+        file_arg[i][0] == os.sep or \
+        file_arg[i][1] == os.sep
+
+dp(path_is_abs)
+dp(os.getcwd())
+
+if path_is_abs is True:
+    path = file_arg[1]
+else:
+    path = os.sep.join([os.getcwd(), file_arg[1]])
+print(path)
+
+for root, subdir, files in os.walk(path, topdown=True):
+    print('-' * 50)
+    print('root:', end='\t')
+    pprint(root)
+    print('subdir:', end='\t')
+    pprint(subdir)
+    print('files:', end='\t')
+    pprint(files)
+    print('\n' * 2 )
+    time.sleep(5)
+
+
+
+
