cedf5e75ef4baaad81a956c7fd3bd38e7c62841a
Kshitij Chawla <kc.insight.pi@gmail.com>
2017-04-22 21:18:49 -0400

    Added inputs() to parse file args from terminal

--- core/get_path.py
+++ core/get_path.py
@@ -18,39 +18,86 @@ def dp(*arg):
     print('-' * 50, 'END', '-' * 50, sep='\n')
 
 
-def paths(file_arg):
+def inputs(file_arg):
+    """
+    Parses the command parameters given and passes them to relevant functions.
+    :param :list(str): file_arg:
+    :return:
+    """
+    
+    if len(file_arg) == 0:
+        print()
+        print("ERROR. Command parameters not registered!")
+        print("Source Code debugging might be necessary.")
+        print("Contact the program author. Include a screenshot and details of the error if possible.")
+        print("Terminating program...")
+        quit()
+    elif len(file_arg) == 1:  # when no path or switches used, and executed command is the only arg.
+        paths(os.getcwd())
+    elif len(file_arg) > 1:
+        if file_arg[1][0] == '-':  # when path not specified but switches used.
+            paths(os.getcwd())
+        else:
+            paths(file_arg[1])
+        
+
+
+def paths(path_arg):
     """
     Gets the inputs given as terminal parameters and processes them.
     :return:
     """
-    print()
-    path_is_abs = 'empty'
-    # for i in range(0, len(file_arg)):
-    if file_arg[1]:
-        print(1)
-        path_is_abs = file_arg[1] == os.sep or \
-                      file_arg[1][0] == os.sep or \
-                      file_arg[1][2] == os.sep  # for when coding on windows
-        
-    if path_is_abs is False:
-        cwd = os.getcwd()
-        if file_arg[1]:
-            path_ = os.sep.join([cwd, file_arg[1]])
+    # dp(path_arg)
+    path_is_abs = path_arg == os.sep or \
+                  path_arg[0] == os.sep or \
+                  path_arg[2] == os.sep  # for when coding on windows
+    path_arg_parts = path_arg.split(os.sep)
+    pardir_idx = list()
+    pardir_count = 0
+    for idx, part_ in enumerate(path_arg_parts):
+        print(idx, part_)
+        if part_ == os.pardir:
+            pardir_idx.append(idx)
+            pardir_count += 1
         else:
-            path_ = cwd
-    elif path_is_abs is True:
-        path_ = file_arg[1]
+            break
+    post_path_parts = path_arg_parts[idx:]
+    cwd = os.getcwd()
+    cwd_parts = cwd.split(os.sep)
+    pre_path_parts = cwd_parts[:-pardir_count]
+    # pre_path_parts = os.sep.join(cwd_parts[:-pardir_count])
+    print('pre_path_parts', pre_path_parts)
+    print('post_path_parts', post_path_parts)
+    pre_path_parts.extend(post_path_parts)
+    print('pre_path_parts', pre_path_parts)
+    path_arg = os.sep.join(pre_path_parts)
     
+    if path_is_abs is False:
+        path_ = os.sep.join([os.getcwd(), path_arg])
+    elif path_is_abs is True:
+        path_ = path_arg
+    
+    
+    #/ for debugging
     print('cwd:', os.getcwd())
-    print('file_arg:', file_arg)
+    print('path_arg:', path_arg)
     print('path_is_abs:', path_is_abs)
     print('path_:', path_)
     print('path_ exists:', os.path.exists(path_))
-    
+    #\
+
+    if os.path.exists(path_) is False:
+        print("Specified path does not exist.")
+        print('Interpreted path:', path_)
+        print("Terminating program...")
+        quit()
     
 def main():
+    print()  ##
     file_arg = sys.argv
-    paths(file_arg)
+    print('file_arg:', file_arg)
+    inputs(file_arg)
+    # paths(file_arg)
 
 if __name__ == '__main__':
     main()
