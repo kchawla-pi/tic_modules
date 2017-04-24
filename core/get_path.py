608e1f38342ff9677f742cc83c96526e6e0193cf
Kshitij Chawla <kc.insight.pi@gmail.com>
2017-04-24 10:13:55 -0400

    Added use(), set_os_type; modded main(); removed resolve_dots_in__path()
     - added use() to run file from nix terminal.
     - added set_os_type to determine run system.
     - modified main() to direct execution to use or test depending on
     output of set_os_type (for nix or win resp)
     - replaced resolve_dots_in_path() with builtin py lib functions
     - refined tests()

--- core/get_path.py
+++ core/get_path.py
@@ -4,6 +4,10 @@ import time
 import sys
 from pprint import pprint
 
+_OS_TYPE = 'nix'
+_COUNT = 0
+_STRICT = True
+
 
 def dp(*arg):
     """
@@ -12,10 +16,12 @@ def dp(*arg):
     :param arg:
     :return: None
     """
-    print('-'*50, 'Start', '-'*50, sep='\n')
+    global _COUNT
+    _COUNT += 1
+    print('.'*5, 'Start', '_COUNT')
     for i in arg:
         print(i)
-    print('-' * 50, 'END', '-' * 50, sep='\n')
+    print('.'*5, 'END', '_COUNT')
 
 
 def inputs(file_arg):
@@ -24,7 +30,7 @@ def inputs(file_arg):
     :param :list(str): file_arg:
     :return:
     """
-    
+    global _STRICT
     if len(file_arg) == 0:
         print()
         print("ERROR. Command parameters not registered!")
@@ -33,116 +39,86 @@ def inputs(file_arg):
         print("Terminating program...")
         quit()
     elif len(file_arg) == 1:  # when no path or switches used, and executed command is the only arg.
-        paths(os.getcwd())
+        final_path_arg = os.getcwd()
     elif len(file_arg) > 1:
         if file_arg[1][0] == '-':  # when path not specified but switches used.
-            paths(os.getcwd())
+            final_path_arg = os.getcwd()
         else:
-            paths(file_arg[1])
+            final_path_arg = file_arg[1]
+    return final_path_arg
          
-    
-def resolve_dots_in_path(path_arg):
-    cwd_parts = os.getcwd().split(os.sep)
-    path_arg_parts = path_arg.split(os.sep)
-    temp = list()
-    for part_ in path_arg_parts:
-        if part_ != os.curdir:
-            temp.append(part_)
-    path_arg_parts = temp.copy()
-    temp = list()
-    for part_ in path_arg_parts:
-        if part_ == os.pardir:
-            cwd_parts.pop()
-        else:
-            temp.append(part_)
-    # path_arg = os.sep.join(temp)
-    cwd_parts.extend(temp)
-    path_ = os.sep.join(cwd_parts)
-    print("path_ in fn:", path_)
-    return path_
-    
 
-def paths(path_arg):
+def paths(final_path_arg):
     """
     Gets the inputs given as terminal parameters and processes them.
     :return:
     """
-    """
-    path_arg == os.sep  :  if os.sep, then at root of *nix system
-    path_arg[0] == os.sep  :  if first character is os.sep, then abs path of *nix os.
-    path_arg[2] == os.sep  # for when coding on windows- root is C:\, for example.
-    """
-    path_is_abs = path_arg == os.sep or \
-                  path_arg[0] == os.sep or \
-                  path_arg[1] == ':'  # for when coding on windows- root is C:\, for example.
-
-    path_ = resolve_dots_in_path(path_arg)
-    
-    if path_is_abs is False:
-        path_ = os.sep.join([os.getcwd(), path_arg])
-    elif path_is_abs is True:
-        path_ = path_arg
-    
-    
-    #/ for debugging
-    # print('-'*50)
-    # print('cwd:', os.getcwd())
-    # print('path_arg:', path_arg)
-    # print('path_is_abs:', path_is_abs)
-    # print('path_:', path_)
-    # print('path_ exists:', os.path.exists(path_))
-    # print('-' * 50)
-    #\
-
+    global _STRICT
+    path_ = os.path.abspath(final_path_arg)
     if os.path.exists(path_) is False:
         print("ERROR. Specified path does not exist.")
         print('Interpreted path:', path_)
-        print("Terminating program...")
-        quit()
+        if _STRICT is False:
+            print("Continuing testing without terminating program.")
+        else:
+            print("Terminating program...")
+            quit()
+    else:
+        return path_
+
+    
+def set_os_type(os_info='nix'):
+    if 'win' in os_info.lower():
+        global _OS_TYPE
+        _OS_TYPE = 'win'
+        os_info = 'win'
+    return os_info
     
     
 def test():
+    global _OS_TYPE
+    global _STRICT
+    _STRICT = False
     test_data_file = "D:\\libraries\\kc\\Dropbox\\workspace\\tic\\kc\\tic_modules\\test_paths_from_sh.txt"
+    print(os.path.abspath(test_data_file))
     with open(test_data_file, 'r') as f_obj:
-        contents = f_obj.readlines()
-    for content_ in contents:
-        content_.rstrip()
-        print(content_)
-        resolve_dots_in_path(content_)
+        file_args = f_obj.readlines()
+    for file_arg_ in file_args:
+        file_arg_ = file_arg_.rstrip()
+        print()
+        print('Input -- test().file_arg_:  ', file_arg_)
+        str2argv = file_arg_.split(sep=' ')
+        resolved_inputs = inputs(str2argv)
+        path_ = paths(resolved_inputs)
+        print('Output -- test().path_:   ', path_)
+        
 
+def use():
+    global _OS_TYPE
+    global _STRICT
+    _STRICT = True
+    print('use', _OS_TYPE)
+    file_arg_ = sys.argv
+    print('use().file_arg_:', file_arg_)
+    inputs(file_arg_)
 
+    
 def main():
-    print()  ##
-    file_arg = sys.argv
-    print('file_arg:', file_arg)
-    inputs(file_arg)
-    # paths(file_arg)
+    global _OS_TYPE
+    os_info = platform.platform()
+    os_info = set_os_type(os_info)
+    _OS_TYPE = os_info
+    if _OS_TYPE == 'win':
+        test()
+    else:
+        use()
+    
 
 if __name__ == '__main__':
-    test()
-    # main()
+    print()
+    main()
+    
 
-#
-#
-# dp(path_is_abs)
-# dp(os.getcwd())
-#
-# if path_is_abs is True:
-#     path = file_arg[1]
-# else:
-#     path = os.sep.join([os.getcwd(), file_arg[1]])
-# print(path)
-#
-# for root, subdir, files in os.walk(path, topdown=True):
-#     print('-' * 50)
-#     print('root:', end='\t')
-#     pprint(root)
-#     print('subdir:', end='\t')
-#     pprint(subdir)
-#     print('files:', end='\t')
-#     pprint(files)
-#     print('\n' * 2 )
-#     time.sleep(5)
 
 
 
